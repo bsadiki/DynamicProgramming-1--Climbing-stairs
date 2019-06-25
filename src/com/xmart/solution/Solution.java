a6c5b85a3671f16e7dfa607316e2630f1899587f
bsadiki <pedrosadiki@gmail.com>
2019-06-25 11:20:55 +0100

    dynamic programing solution

--- src/com/xmart/solution/Solution.java
+++ src/com/xmart/solution/Solution.java
@@ -2,15 +2,21 @@ package com.xmart.solution;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(possiblities(5));
+        System.out.println(possiblities(4));
     }
-    static int possiblities(int n){
-        if(n == 0)
+    static int possiblities(int n) {
+        if (n == 0)
             return 0;
         if (n == 1)
             return 1;
         if (n == 2)
             return 2;
-        return possiblities(n-1) + possiblities(n-2);
+        int solutions[] = new int[n];
+        solutions[0] = 1;
+        solutions[1] = 2;
+        for (int i = 2; i < n; i++) {
+            solutions[i] = solutions[i-1] + solutions[i-2];
+        }
+        return solutions[n-1];
     }
 }
